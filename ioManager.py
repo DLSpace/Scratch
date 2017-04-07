64c9539ddfe64990a4b2090360990d2b90f7dbed
spottur2 <sreedhar_pottur@optum.com>
2017-04-07 13:44:15 -0400

    Adding io manager and removing py cache

--- ioManager.py
+++ ioManager.py
@@ -1,5 +1,4 @@
-# These are all the modules we'll be using later. Make sure you can import them
-# before proceeding further.
+## Module to group all io capabilities
 import json
 
 def readJsonF(fileName):

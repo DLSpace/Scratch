54eb00cf13acc7eaaf39807284ae8d8b3880182f
sreedhar potturi <spotturi@localhost.localdomain>
2017-12-13 16:10:10 -0500

    added command line args

--- kerashw.py
+++ kerashw.py
@@ -1,77 +1,108 @@
-import numpy as np
+"""
+test modules
+"""
+import argparse
+import datetime
+import math
+# import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-import math
 import matplotlib.pyplot as plt
-import datetime
+
+ARGPARSER = argparse.ArgumentParser()
+ARGPARSER.add_argument("-f", "--fileName", help="model file name to load")
+ARGPARSER.add_argument("-e", "--epochs", help="number of epochs to train", type=int)
+ARGPARSER.add_argument("-b", "--batchSize", help="batch size to use for SGD", type=int)
+ARGPARSER.add_argument("-n", "--neuronCount", help="number of neurons in a layer", type=int)
+ARGPARSER.add_argument("-y", "--layerCount", help="number of layers in the net", type=int)
+CMD_ARGS = ARGPARSER.parse_args()
+
+# module level state variables
+MODEL = Sequential()
 
 
-model = Sequential()
-#input layer
-model.add(Dense(units=1, input_dim=1))
-model.add(Activation('linear'))
-#hidden layer 1
-model.add(Dense(units=10))
-model.add(Activation('sigmoid'))
-#hidden layer 2
-model.add(Dense(units=10))
-model.add(Activation('sigmoid'))
-#output layer
-model.add(Dense(units=1))
-model.add(Activation('linear'))
+def initialize_net():
+    """
+        Initialize model and setup the layers
+    """
+    # input layer
+    MODEL.add(Dense(units=1, input_dim=1))
+    MODEL.add(Activation('linear'))
+    # hidden layer 1
+    MODEL.add(Dense(units=10))
+    MODEL.add(Activation('relu'))
+    # hidden layer 2
+    MODEL.add(Dense(units=10))
+    MODEL.add(Activation('relu'))
+    # output layer
+    MODEL.add(Dense(units=1))
+    MODEL.add(Activation('linear'))
+    MODEL.compile(loss='mean_squared_error',
+                  optimizer='sgd', metrics=['accuracy'])
 
-model.compile(loss='mean_squared_error',
-              optimizer='sgd',
-              metrics=['accuracy'])
+
+DATA = []
+LABELS = []
 
 
-# Generate dummy data
-import numpy as np
-# data = np.random.random((1000, 1))
-data = []
-labels = [];
-for i in range(0,90,1):
-    data.extend([[math.radians(i)]])
-    labels.extend([[math.sin(math.radians(i))]])
-# data = np.array([[0],
-#                 [0.523598776],
-#                 [0.785398163],
-#                 [1.047197551],
-#                 [1.570796327]])
-# print(data)
-# labels = np.random.randint(2, size=(5, 1))
+def initialize_training_set():
+    """
+    intialize training set and labels
+    """
+    for i in range(0, 90, 1):
+        DATA.extend([[math.radians(i)]])
+        LABELS.extend([[math.sin(math.radians(i))]])
 
-# labels = np.array([[0],
-#                 [0.5],
-#                 [0.707106781],
-#                 [0.866025404],
-#                 [1]])
-# print(labels)
+
+def save_model(file_name):
+    """
+    Saves a trained model definition and weights to file
+    """
+    model_json = MODEL.to_json()
+    with open(file_name + ".json", "w") as json_file:
+        json_file.write(model_json)
+    MODEL.save_weights(file_name + ".h5")
+
+
+def test_model():
+    """
+    Test the model and check how accurate predictions are
+    """
+    test_data = []
+    for i in range(0, 180, 2):
+        test_data.extend([[math.radians(i)]])
+    pred = MODEL.predict(test_data, batch_size=1)
+    fig, ax = plt.subplots()
+    plt.ylabel('Predict')
+    plt.xlabel(datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
+    plt.ioff()
+    plt.autoscale(enable=True, axis='both')
+    for i in range(0, len(pred), 1):
+        # ax.plot(epoch,self.avgCost,'.');
+        ax.plot(test_data[i][0], pred[i][0], 'b,')
+        ax.plot(test_data[i][0], math.sin(test_data[i][0]), 'r,')
+        print('sin(' + str(test_data[i][0]) + ') = ' +
+        str(pred[i][0]) + ' :act: ' + str(math.sin(test_data[i][0])))
+    plt.savefig("kerastest.png")
+    plt.close()
+
+
+if CMD_ARGS.fileName is not None:
+        # load model
+    print(CMD_ARGS.fileName + " specified")
+else:
+    initialize_net()
+
+EPOCHS = 2000
+if CMD_ARGS.epochs is not None:
+    EPOCHS = CMD_ARGS.epochs
+
+BATCH_SIZE=5
+if CMD_ARGS.batchSize is not None:
+    BATCH_SIZE = CMD_ARGS.batchSize
+
+initialize_training_set()
 # Train the model, iterating on the data in batches of 32 samples
-model.fit(data, labels, epochs=2000, batch_size=5)
-
-model_json = model.to_json()
-with open("kerastestModel.json", "w") as json_file:
-    json_file.write(model_json)
-model.save_weights("kerastestModel.h5")
-
-testData = [];
-for i in range(0,180,2):
-    testData.extend([[math.radians(i)]])
-
-pred = model.predict(testData,batch_size=1)
-
-fig, ax = plt.subplots()
-plt.ylabel('Predict');
-plt.xlabel(datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"));
-plt.ioff()
-plt.autoscale(enable=True,axis='both');
-
-for i in range(0,len(pred),1):
-    # ax.plot(epoch,self.avgCost,'.');
-    ax.plot(testData[i][0],pred[i][0],'b,');
-    ax.plot(testData[i][0],math.sin(testData[i][0]),'r,');
-    print('sin('+str(testData[i][0])+') = '+str(pred[i][0])+' :act: ' + str(math.sin(testData[i][0])))
-
-plt.savefig("kerastest.png")
-plt.close()
+MODEL.fit(DATA, LABELS, epochs=EPOCHS, batch_size=BATCH_SIZE)
+test_model()
+save_model("kerasSinModel")
--- layer.py
+++ layer.py
@@ -13,127 +13,131 @@ logging.basicConfig(level=logging.WARNING)
 
 
 class Layer(object):
-	"""Class that defines a layer in the nnet"""
-	
+    """Class that defines a layer in the nnet"""
 
-	def initializeState(self, kwargs):
-		"""
-			Initalizer expects three constructor params.
-			neuroncount			-	Number of neurons in this layer
-			inputNeuronCount	-	Number of neurons in the preceeding layer, zero if this is the input layer
-			sharedBias			-	Wether to share a single bias value for the whole layer or to have each neuron to 
-			import layer
-			from layer import Layer
-			il = Layer(neurons=2,inputNeurons=0)
-			l1 = Layer(neurons=2,inputNeurons=2)
-			ol = Layer(neurons=2,inputNeurons=2)
-		"""
-		logging.info(kwargs);
-		if kwargs.get('neurons') is not None:
-			logging.info("neuron count is %s" % (kwargs['neurons']));
-			self.neuronCount = int(kwargs['neurons']);
-			self.activations = T.zeros(shape=[self.neuronCount]);
-		if kwargs.get('inputNeurons') is not None:
-			logging.info("inputNeurons count is %s" % (kwargs['inputNeurons']));
-			self.inputNeuronCount = int(kwargs['inputNeurons']);
-		if kwargs.get('sharedBias') is not None:
-			logging.info("Shared Bias is %s" % (kwargs['sharedBias']));
-			self.sharedBias = bool(kwargs['sharedBias']);
-		if kwargs.get('inputLayer') is not None:
-			logging.info("input layer is %s" % (kwargs['inputLayer']));
-			self.inputLayer = kwargs['inputLayer'];
-		if kwargs.get('name') is not None:
-			logging.info("name is %s" % (kwargs['name']));
-			self.name = kwargs['name'];
+    def initializeState(self, kwargs):
+        """
+                Initalizer expects three constructor params.
+                neuroncount			-	Number of neurons in this layer
+                inputNeuronCount	-	Number of neurons in the preceeding layer, zero if this is the input layer
+                sharedBias			-	Wether to share a single bias value for the whole layer or to have each neuron to 
+                import layer
+                from layer import Layer
+                il = Layer(neurons=2,inputNeurons=0)
+                l1 = Layer(neurons=2,inputNeurons=2)
+                ol = Layer(neurons=2,inputNeurons=2)
+        """
+        logging.info(kwargs)
+        if kwargs.get('neurons') is not None:
+            logging.info("neuron count is %s" % (kwargs['neurons']))
+            self.neuronCount = int(kwargs['neurons'])
+            self.activations = T.zeros(shape=[self.neuronCount])
+        if kwargs.get('inputNeurons') is not None:
+            logging.info("inputNeurons count is %s" % (kwargs['inputNeurons']))
+            self.inputNeuronCount = int(kwargs['inputNeurons'])
+        if kwargs.get('sharedBias') is not None:
+            logging.info("Shared Bias is %s" % (kwargs['sharedBias']))
+            self.sharedBias = bool(kwargs['sharedBias'])
+        if kwargs.get('inputLayer') is not None:
+            logging.info("input layer is %s" % (kwargs['inputLayer']))
+            self.inputLayer = kwargs['inputLayer']
+        if kwargs.get('name') is not None:
+            logging.info("name is %s" % (kwargs['name']))
+            self.name = kwargs['name']
 
-	def randomizeWeightsandBiases(self):
-		"""
-			n = neurons
-			in = input layer neuron count
-			a = [1 X n] row matrix
-			w = [in X n] 2D matrix where weights from one input neron to next layer are in the column
-			and weights from all input neurons are in a row
-			--
-			|  l
-			| w
-			|  jk
-			--
-			J is in current layer so the weight arrow is going from k --> j
-			b = [1 X n] row matrix
+    def randomizeWeightsandBiases(self):
+        """
+                n = neurons
+                in = input layer neuron count
+                a = [1 X n] row matrix
+                w = [in X n] 2D matrix where weights from one input neron to next layer are in the column
+                and weights from all input neurons are in a row
+                --
+                |  l
+                | w
+                |  jk
+                --
+                J is in current layer so the weight arrow is going from k --> j
+                b = [1 X n] row matrix
 
-		"""
-		logging.info("name : " + self.name)
-		#randomizing weights
-		if self.inputNeuronCount>0:
-			#randWeights = self.sharedRandomStream.normal([self.inputNeuronCount,self.neuronCount]).eval().copy();
-			self.weights =  th.shared(value=(self.sharedRandomStream.normal([self.inputNeuronCount,self.neuronCount]).eval().copy()),name='w');
-			logging.info("weights intialized")
-		if self.sharedBias:
-			logging.info("using shared bias.");
-			randBiases = self.sharedRandomStream.normal([1]).eval().copy();
-			self.biases = th.shared(value=randBiases,name='b');
-		else:
-			randBiases = self.sharedRandomStream.normal([self.neuronCount]).eval().copy();
-			self.biases = th.shared(value=randBiases,name='b');
-		logging.info("biases intialized")
+        """
+        logging.info("name : " + self.name)
+        # randomizing weights
+        if self.inputNeuronCount > 0:
+            #randWeights = self.sharedRandomStream.normal([self.inputNeuronCount,self.neuronCount]).eval().copy();
+            self.weights = th.shared(value=(self.sharedRandomStream.normal(
+                [self.inputNeuronCount, self.neuronCount]).eval().copy()), name='w')
+            logging.info("weights intialized")
+        if self.sharedBias:
+            logging.info("using shared bias.")
+            randBiases = self.sharedRandomStream.normal([1]).eval().copy()
+            self.biases = th.shared(value=randBiases, name='b')
+        else:
+            randBiases = self.sharedRandomStream.normal(
+                [self.neuronCount]).eval().copy()
+            self.biases = th.shared(value=randBiases, name='b')
+        logging.info("biases intialized")
 
-	def __init__(self, **kwargs):
-		self.neuronCount = 2;
-		self.inputNeuronCount = 0;
-		self.sharedBias = False;
-		self.sharedRandomStream = RandomStreams(seed=int(round(time.time())));
-		self.expected = T.fscalar('e');
-		self.learningRate = 2.5;
-		self.initializeState(kwargs)
-		self.randomizeWeightsandBiases()
-		self.calculateActivations()
-		return
+    def __init__(self, **kwargs):
+        self.neuronCount = 2
+        self.inputNeuronCount = 0
+        self.sharedBias = False
+        self.sharedRandomStream = RandomStreams(seed=int(round(time.time())))
+        self.expected = T.fscalar('e')
+        self.learningRate = 2.5
+        self.initializeState(kwargs)
+        self.randomizeWeightsandBiases()
+        self.calculateActivations()
+        return
 
-	def calculateActivations(self):
-		if self.inputNeuronCount>0:
-			"""
-				T.dot(a,b) is sum product of last access of a and 2nd to last access of b
-			"""
-			self.activations =  T.nnet.sigmoid(T.dot(self.inputLayer.activations,self.weights)+self.biases);
-		return self.activations;
+    def calculateActivations(self):
+        if self.inputNeuronCount > 0:
+            """
+                    T.dot(a,b) is sum product of last access of a and 2nd to last access of b
+            """
+            self.activations = T.nnet.sigmoid(
+                T.dot(self.inputLayer.activations, self.weights) + self.biases)
+        return self.activations
 
-	def cost(self):
-		return ((T.sqr(T.sub(self.activations, self.expected)).sum())/self.neuronCount);
+    def cost(self):
+        return ((T.sqr(T.sub(self.activations, self.expected)).sum()) / self.neuronCount)
 
-	def sgd(self):
-		self.gw = T.grad(cost=self.cost(),wrt=self.weights);
-		self.gb = T.grad(cost=self.cost(),wrt=self.biases);
-		#TODO : parameterize learning rate
-		self.updates = [	(self.weights,self.weights - float(self.learningRate) *  self.gw),
-					(self.biases,self.biases - float(self.learningRate) * self.gb)
-			 ];
-		self.sgd_func = th.function(
-			inputs = [self.expected],
-			outputs = self.cost(),
-			updates = self.updates
-			#givens = {expected : exp}
-			);
-		#print(gw.eval())
-		#print(gb.eval())
-		return self.sgd_func
+    def sgd(self):
+        self.gw = T.grad(cost=self.cost(), wrt=self.weights)
+        self.gb = T.grad(cost=self.cost(), wrt=self.biases)
+        # TODO : parameterize learning rate
+        self.updates = [	(self.weights, self.weights - float(self.learningRate) * self.gw),
+                         (self.biases, self.biases -
+                          float(self.learningRate) * self.gb)
+                         ]
+        self.sgd_func = th.function(
+            inputs=[self.expected],
+            outputs=self.cost(),
+            updates=self.updates
+            #givens = {expected : exp}
+        )
+        # print(gw.eval())
+        # print(gb.eval())
+        return self.sgd_func
 
 
 class LayerTest(unittest.TestCase):
-	def test_init(self):
-		l1 = Layer(neurons=4,inputNeurons=4,sharedBias=False)
-		self.assertEqual(l1.neuronCount,4)
-		self.assertEqual(l1.inputNeuronCount,4)
-		self.assertFalse(l1.sharedBias)
-		self.assertEqual(np.shape(l1.biases),(l1.neuronCount,))
-		self.assertEqual(np.shape(l1.weights),(l1.neuronCount,l1.inputNeuronCount))
+    def test_init(self):
+        l1 = Layer(neurons=4, inputNeurons=4, sharedBias=False)
+        self.assertEqual(l1.neuronCount, 4)
+        self.assertEqual(l1.inputNeuronCount, 4)
+        self.assertFalse(l1.sharedBias)
+        self.assertEqual(np.shape(l1.biases), (l1.neuronCount,))
+        self.assertEqual(np.shape(l1.weights),
+                         (l1.neuronCount, l1.inputNeuronCount))
 
-	def test_layer_zero(self):
-		l1 = Layer(neurons=4,inputNeurons=0,sharedBias=False)
-		self.assertEqual(l1.neuronCount,4)
-		self.assertEqual(l1.inputNeuronCount,0)
-		self.assertFalse(l1.sharedBias)
-		self.assertEqual(np.shape(l1.biases),(l1.neuronCount,))
-		self.assertEqual(np.shape(l1.weights),(l1.inputNeuronCount,))
+    def test_layer_zero(self):
+        l1 = Layer(neurons=4, inputNeurons=0, sharedBias=False)
+        self.assertEqual(l1.neuronCount, 4)
+        self.assertEqual(l1.inputNeuronCount, 0)
+        self.assertFalse(l1.sharedBias)
+        self.assertEqual(np.shape(l1.biases), (l1.neuronCount,))
+        self.assertEqual(np.shape(l1.weights), (l1.inputNeuronCount,))
 
 
 if __name__ == '__main__':
